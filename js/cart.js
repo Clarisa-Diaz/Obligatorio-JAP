3ffabc91b6ab08e3d0c6301010da2556f0fc973a
Clarisa Diaz <clarisa_evangelina@hotmail.com>
2019-11-13 10:45:14 -0300

    ultima entrega

--- js/cart.js
+++ js/cart.js
@@ -12,33 +12,42 @@ let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
 function updateSubtotal(){
     let precioUni = parseInt(document.getElementById("precio").textContent);
     let cant = parseInt(document.getElementById("cantidad").value);
-    let subTo = cant*precioUni;
-    document.getElementById("subtotal").innerHTML = subTo;
+    subtotal = cant*precioUni;
+    document.getElementById("subTotal").innerHTML = subtotal;
+    document.getElementById("productCostText").innerHTML = subtotal;
 }
 
 
 function showArticles(array){
     let contenido = "";
-    for (let i = 0; i < array.length; i++){
+    for (let i = 0; i<array.length; i++){
         articulo = array[i];
     contenido += `
         <tr>
-            <td><img src="` + articulo.src + `" width="50px"></td>
-            <td>`+ articulo.name +`</td>
-            <td id="precio">`+ articulo.unitCost +`</td>
-            <td><input type="number" name="count" id="cantidad" value=`+ articulo.count +`</td>
-            <td>`+ articulo.currency + `</td>
-            <td><span id="subtotal"></span></td>
+            <td><img src="`+articulo.src+`" width="50px"></td>
+            <td>`+articulo.name+`</td>
+            <td id="precio">`+articulo.unitCost+`</td>
+            <td><input type="number" name="count" id="cantidad" value="`+articulo.count+`"</td>
+            <td>`+articulo.currency+`</td>
+            <td><span id="subTotal"></span></td>
         </tr>
     `
     document.getElementById("infoTable").innerHTML = contenido;
     updateSubtotal(); 
-
+    updateTotalCosts();
     document.getElementById("cantidad").addEventListener("change", function(){
         updateSubtotal();
         })
     }
 }
+
+function updateTotalCosts(){
+    let costoEnvio = subtotal * shippingPercentage;
+    document.getElementById("comissionText").innerHTML = costoEnvio;
+    total = subtotal + costoEnvio;
+    document.getElementById("totalCostText").innerHTML = total;
+} 
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
